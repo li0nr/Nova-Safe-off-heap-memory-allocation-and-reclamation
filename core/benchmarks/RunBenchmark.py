9f4ef025459da814b9223a5ce235fdf2e4f47c7e
Ramy Fakhoury <ramy.f@campus.technion.ac.il>
2021-01-01 18:48:58 +0200

    benchmark script update

--- core/benchmarks/RunBenchmark.py
+++ core/benchmarks/RunBenchmark.py
@@ -1,39 +1,28 @@
 import subprocess
-import os
 import time
 
-print(__name__)
-os.chdir(os.path.abspath(os.path.expanduser('../target/classes')))
-Lists = ["N"]
+Lists = ["N","U"]
 Modes = ["R","W"]
-Threads = ["1","2","4"]#,"8","12","16","20","24","28","32"]
-
+Threads = ["1","2","4","8","12","16","20","24","28","32"]
 for list in Lists:
 	for mode in Modes:
-		file1  = open("results"+mode+list, "a")
-		for thread in Threads:
-			args = ["java","-server", "-Xmx64g","-XX:MaxDirectMemorySize=4g", "com.yahoo.oak.SequntialBenchmark",list,mode,thread,"100"]
-			print("testsing ",args)
-			p = subprocess.run(args, stdout=subprocess.PIPE) # Success!
-			time.sleep( 5 )
-			file1.write(p.stdout.decode('ASCII'))
-			print(p.stdout.decode('ASCII'))
-		print("changeing modes")
-		file1.close()
-	print("changeing lists")
-	file1.close()
-
+            for thread in Threads:
+                file1  = open("results"+mode+list, "a")
+            #p=os.system('java -cp ../nova-0.0.1-SNAPSHOT.jar -server -Xmx64g -XX:MaxDirectMemorySize=4g  com.yahoo.oak.SequntialBenchmark  N R 1 50000000&')
+                args = ["java","-cp","../nova-0.0.1-SNAPSHOT.jar","-server", "-Xmx64g","-XX:MaxDirectMemorySize=4g", "com.yahoo.oak.SequntialBenchmark",list,mode,thread,"50000000"]
+                print("testsing ",args)
+                p = subprocess.run(args, stdout=subprocess.PIPE) # Success!
+                time.sleep( 2 )
+                file1.write(p.stdout.decode('ASCII'))
+                print(p.stdout.decode('ASCII'))
 for list in Lists:
 	for mode in Modes:
-		file1  = open("results"+mode+list+"rand", "a")
-		for thread in Threads:
-			args = ["java","-server", "-Xmx64g","-XX:MaxDirectMemorySize=4g", "com.yahoo.oak.RandomBenchmark",list,mode,thread,"100"]
-			print("testsing ",args)
-			p = subprocess.run(args, stdout=subprocess.PIPE) # Success!
-			time.sleep( 5 )
-			file1.write(p.stdout.decode('ASCII'))
-			print(p.stdout.decode('ASCII'))
-		print("changeing modes")
-		file1.close()
-	print("changeing lists")
-	file1.close()
+            for thread in Threads:
+                file1  = open("results"+mode+list, "a")
+            #p=os.system('java -cp ../nova-0.0.1-SNAPSHOT.jar -server -Xmx64g -XX:MaxDirectMemorySize=4g  com.yahoo.oak.SequntialBenchmark  N R 1 50000000&')
+                args = ["java","-cp","../nova-0.0.1-SNAPSHOT.jar","-server", "-Xmx64g","-XX:MaxDirectMemorySize=4g", "com.yahoo.oak.RandomBenchmark",list,mode,thread,"50000000"]
+                print("testsing ",args)
+                p = subprocess.run(args, stdout=subprocess.PIPE) # Success!
+                time.sleep( 2 )
+                file1.write(p.stdout.decode('ASCII'))
+                print(p.stdout.decode('ASCII'))
