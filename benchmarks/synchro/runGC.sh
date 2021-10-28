bba29bcf8e447f30e8facb7cd209f7783dafb123
Ramy Fakhoury <li0nr208@gmail.com>
2021-10-28 10:17:35 +0300

    added GC benchamrk script

---
+++ benchmarks/synchro/runGC.sh
@@ -0,0 +1,257 @@
+#!/usr/bin/env bash
+# trap ctrl-c and call ctrl_c()
+trap ctrl_c INT
+CONTINUE=1
+
+function ctrl_c() {
+  echo "#### User enter CTRL-C"
+  CONTINUE=0
+}
+
+############################################################################
+# All test scenarios and benchmarks
+############################################################################
+declare -A scenarios=(
+  ["25Put25Delete50Get"]="-a 25 -u 50 -s 50"
+)
+
+declare -A benchmarks=(
+  ["skip-list"]="SkipList_OnHeap"
+  ["offheap-list-key"]="SkipList_OffHeap"
+  ["offheap-list-key-value"]="SkipList_OffHeap_Keys"
+
+
+)
+
+declare -A benchmark_Size=(
+  ["mem01_G"]="1000000"
+  ["mem10_G"]="10000000"
+  ["mem30_G"]="30000000"
+  ["mem50_G"]="50000000"
+  ["mem80_G"]="80000000"
+  ["mem90_G"]="90000000"
+)
+
+
+declare -A gc_cmd_args=(
+  #["default"]=""
+  ["Serial"]="-XX:+UseSerialGC"
+  ["Parallel"]="-XX:+UseParallelGC"
+  ["CMS"]="-XX:+UseParNewGC"
+  ["g1"]="-XX:+UseG1GC" #default
+  ["zgc"]="-XX:+UseZGC"
+)
+
+
+
+############################################################################
+# Default arguments
+############################################################################
+java=java
+
+# Default output is <pwd>/output.
+output=$(pwd)/output
+
+# Automatically picks the correct synchrobench JAR file
+jar_file_path=$(find "$(pwd)" -name "oak-benchmarks-synchrobench-*.jar" | grep -v "javadoc" | xargs ls -1t | head -1)
+# Pipes breakdown:
+#  1. find the synchrobench JAR file (show full path)
+#  2. exclude the JAR with the javadoc
+#  3. sort by modification date (most recent first)
+#  4. take the first result (the most recent JAR file)
+
+# Iterate on the cartesian product of these arguments (space separated)
+test_scenarios=${!scenarios[*]}
+test_benchmarks=${!benchmarks[*]}
+test_size=${!benchmark_Size[*]}
+test_thread="08 16"
+test_gc="default"
+test_java_modes="server"
+
+# Defines the key size
+keysize="128"
+
+# Defines the value size
+valuesize="1024"
+
+# Defines the number of warm-up (not measured) iterations.
+warmup="30"
+
+# Defines the number of measured iterations.
+iterations="3"
+
+# Defines the test runtime in milliseconds.
+duration="30000"
+
+# Defines the sampling range for queries and insertions.
+range_ratio="2"
+
+# This flag is used to debug the script before a long execution.
+# If set to "1" (via "-v" flag in the command line), the script will produces all the output files (with all the runtime
+# parameters), but will not run the benchmark command.
+# Usage: when modifying the script for a specific test and running it for a long period (e.g., overnight),
+# it is recommended to verify that all the parameters are as intended, and that the scripts will not fail for some
+# reason after a few iterations only to be discovered in the morning.
+verify_script=0
+
+benchClassPrefix="com.yahoo.oak"
+
+############################################################################
+# Override default arguments
+############################################################################
+while getopts o:j:d:i:w:s:t:e:h:b:g:m:l:r:v opt; do
+  case ${opt} in
+  o) output=$OPTARG ;;
+  j) java=$OPTARG ;;
+  d) duration=$((OPTARG * 1000)) ;;
+  i) iterations=$OPTARG ;;
+  w) warmup=$OPTARG ;;
+  h)
+    for bench in ${!heap_limit[*]}; do
+      heap_limit[${bench}]=$OPTARG
+    done
+    ;;
+  l)
+    for bench in ${!direct_limit[*]}; do
+      direct_limit[${bench}]=$OPTARG
+    done
+    ;;
+  r) range_ratio=$OPTARG ;;
+  s) test_size=$OPTARG ;;
+  t) test_thread=$OPTARG ;;
+  e) test_scenarios=$OPTARG ;;
+  b) test_benchmarks=$OPTARG ;;
+  g) test_gc=$OPTARG ;;
+  m) test_java_modes=$OPTARG ;;
+  v) verify_script=1 ;;
+  \?)
+    echo "Invalid Option: -$OPTARG" 1>&2
+    exit 1
+    ;;
+  :)
+    echo "Invalid Option: -$OPTARG requires an argument" 1>&2
+    exit 1
+    ;;
+  esac
+done
+
+############################################################################
+# Changing working directory to the JAR file directory
+############################################################################
+synchrobench_path=$(dirname "${jar_file_path}")
+jar_file_name=$(basename "${jar_file_path}")
+echo "Found synchrobench JAR in: ${synchrobench_path}"
+echo "Using JAR: ${jar_file_name}"
+cd "${synchrobench_path}" || exit 1
+
+############################################################################
+# Initialized output folder
+############################################################################
+if [[ ! -d "${output:?}" ]]; then
+  mkdir -p "${output}"
+fi
+
+timestamp=$(date '+%d-%m-%Y--%H-%M-%S')
+summary="${output}/summary-${timestamp}.csv"
+
+echo "Timestamp, Log File, Scenario, Bench, Heap size, Direct Mem, # Threads, GC, Final Size, Throughput, std" >"${summary}"
+
+echo "Starting benchmark: $(date)"
+
+# Iterate over a cartesian product of the arguments
+for scenario in ${test_scenarios[*]}; do for bench in ${test_benchmarks[*]}; do
+  echo ""
+  echo "Scenario: ${bench} ${scenario}"
+  echo "" >>"${summary}"
+
+  scenario_args=${scenarios[${scenario}]}
+  classPath="${benchClassPrefix}.${benchmarks[${bench}]}"
+
+    for java_mode in ${test_java_modes[*]}; do for gc_alg in ${test_gc[*]}; 
+	do  for thread in ${test_thread[*]}; do for size in ${test_size[*]}; do
+        # Check if the user hit CTRL+C before we start a new iteration
+        if [[ "$CONTINUE" -ne 1 ]]; then
+          echo "#### Quiting..."
+          exit 1
+        fi
+
+        gc_args=${gc_cmd_args[${gc_alg}]}
+        java_args="${java_modes[${java_mode}]} ${gc_args}"
+
+        # Set the range to a factor of the size of the data
+        range=$((range_ratio * size))
+
+        # Add a timestamp prefix to the log file.
+        # This allows repeating the benchmark with the same parameters in the future without removing the old log.
+        timestamp=$(date '+%d-%m-%Y--%H-%M-%S')
+        log_filename=${timestamp}-${scenario}-${bench}-xmx${heapSize}-direct${directSize}-t${thread}-m${java_mode}-gc${gc_alg}.log
+        out=${output}/${log_filename}
+
+        # Construct the command line as a multi-lined list for aesthetics reasons
+        cmd_args=(
+          "${java} ${java_args} -jar ${jar_file_name} -b ${classPath} ${scenario_args}"
+          "-k ${keysize} -v ${valuesize} -i ${size} -r ${range} -t ${thread}"
+          "-W ${warmup} -n ${iterations} -d ${duration}"
+        )
+        cmd=${cmd_args[*]}
+        echo "${cmd}"
+
+        # Print all arguments to the log file
+        {
+          echo "[Arguments]"
+          # General arguments:
+          echo "timestamp: ${timestamp}"
+          echo "log_filename: ${log_filename}"
+          echo "synchrobench_path: ${synchrobench_path}"
+          # Iteration arguments:
+          echo "scenario: ${scenario}"
+          echo "bench: ${bench}"
+          echo "heap_limit: ${heapSize}"
+          echo "direct_limit: ${directSize}"
+          echo "gc_alg: ${gc_alg}"
+          echo "gc_args: ${gc_args}"
+          echo "java_mode: ${java_mode}"
+          echo "write: ${write}"
+          # CMD arguments:
+          echo "cmd: ${cmd}"
+          echo "java: ${java}"
+          echo "java_args: ${java_args}"
+          echo "jar_file_name: ${jar_file_name}"
+          echo "classPath: ${classPath}"
+          echo "scenario_args: ${scenario_args}"
+          echo "keysize: ${keysize}"
+          echo "valuesize: ${valuesize}"
+          echo "warmup: ${warmup}"
+          echo "iterations: ${iterations}"
+          echo "size: ${size}"
+          echo "range: ${range}"
+          echo "thread: ${thread}"
+          echo "duration: ${duration}"
+          echo ""
+          # The benchmark output will be appended here
+          echo "[Output]"
+        } >"${out}"
+
+        if [[ "$verify_script" -ne 1 ]]; then
+          ${cmd} >>"${out}" 2>&1
+
+          # Read statistics from the output log
+          finalSize=$(grep "Mean Total Size:" "${out}" | cut -d : -f2 | tr -d '[:space:]')
+          throughput=$(grep "Mean:" "${out}" | cut -d : -f2 | tr -d '[:space:]')
+          std=$(grep "Standard deviation pop:" "${out}" | cut -d : -f2 | tr -d '[:space:]')
+        fi
+
+        # Update summary
+        summary_line=("${timestamp}" "${log_filename}" "${scenario}" "${bench}" "${heapSize}" "${directSize}"
+          "${thread}" "${gc_alg}" "${finalSize}" "${throughput}" "${std}")
+        (
+          # Define the separator to be a comma instead of a space
+          # This only have effect in the context of these parenthesis
+          IFS=,
+          echo "${summary_line[*]}"
+        ) >>"${summary}"
+      done; done
+    done; done; done
+  done;
+
+echo "Oak test complete $(date)"
